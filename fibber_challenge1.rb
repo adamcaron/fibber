b129dd1b9bd76fc2c9254d5ec42035620a750865
Adam Caron <adamwcaron@gmail.com>
2015-05-23 19:00:32 -0600

    generates quanitity of numbers in fibonacci sequence and prints result

---
+++ fibber_challenge1.rb
@@ -0,0 +1,13 @@
+# start with two elements: 0 and 1
+# Add them together and, based on the sum, create a 3rd element at the end of the sequence
+# Until the total # of elements == 15 (quantity)
+# Print or puts all the elements, displaying them 5 elements per line.
+
+quantity = 15
+
+sequence = [0,1]
+until sequence.count == 15
+  next_number = sequence.last(2).inject(:+)
+  sequence << next_number
+end
+puts sequence
