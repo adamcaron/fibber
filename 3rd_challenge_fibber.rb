1d321b314ded1144f6d9044dbeca0f501abcede6
Adam Caron <adamwcaron@gmail.com>
2015-05-24 16:42:55 -0600

    changes comment documentation

--- 3rd_challenge_fibber.rb
+++ 3rd_challenge_fibber.rb
@@ -1,13 +1,10 @@
-# Can you output your sequence
-# such that each element is padded to a string
-# with a size one greater than the number of digits in the last element that will be output?
+# Output your sequence such that each element is padded to a string
+# with a size one greater than the number of digits in the last element that will be output.
 # So if the biggest number you'll output is three digits,
 # each element of the sequence is padded to four places.
 # But if the last element is four digits, each element is padded to five places.
 
-
 quantity = 150
-
 sequence = [0, 1]
 
 until sequence.count == quantity
@@ -15,10 +12,10 @@ until sequence.count == quantity
   sequence << next_number
 end
 
-# Destructive method: manipulates the array using '.shift' until the array is empty
 until sequence.empty?
   5.times do
     padding = sequence[-1].to_s.length + 1
+    # Destructive method: manipulates the array using '.shift' until the array is empty
     print sequence.shift.to_s.rjust(padding, ' ')
   end
   puts "\n"
